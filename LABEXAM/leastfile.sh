981667817d4d4f0abc20ed1adde2c470cc6ab436
Sudeshna Bhushan <sudeshna.cs19@bmsce.ac.in>
2021-12-08 23:18:05 +0530

    Update leastfile.sh

--- LABEXAM/leastfile.sh
+++ LABEXAM/leastfile.sh
@@ -7,4 +7,7 @@ cat $1
 elif [ $c2 -lt $c1 -a $c2 -lt $c3 ] ; then
 echo "File $2 has the least word count which is equal to $c2"
 cat $2
+else
+echo "File $3 has the least word count which is equal to $c3"
+cat $3
 fi
