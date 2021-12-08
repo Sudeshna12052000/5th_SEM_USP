d5c6d0562665ab89794fe31f60ee22f9d7b1f20b
Sudeshna Bhushan <sudeshna.cs19@bmsce.ac.in>
2021-12-08 23:16:59 +0530

    Create leastfile.sh

---
+++ LABEXAM/leastfile.sh
@@ -0,0 +1,10 @@
+c1=`wc -w < $1`
+c2=`wc -w < $2`
+c3=`wc -w < $3`
+if [ $c1 -lt $c2 -a $c1 -lt $c3 ] ; then
+echo "File $1 has the least word count which is equal to $c1"
+cat $1
+elif [ $c2 -lt $c1 -a $c2 -lt $c3 ] ; then
+echo "File $2 has the least word count which is equal to $c2"
+cat $2
+fi
